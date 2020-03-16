8ccc50efd90ecfe4365cb1d5ea93be9f9b624e46
cgonzaro <cgonzaro@SVQ-2SCC3M2.usersad.everis.int>
2020-03-16 11:42:37 +0100

    Calculo Velocidad added

--- src/logic/OperacionesCoche.java
+++ src/logic/OperacionesCoche.java
@@ -4,10 +4,20 @@ import model.Coche;
 
 public class OperacionesCoche {
 
+	
 	public static double calculoAutonomia(Coche coche) {
 		// Fórmula autonomía: 7 litros a los 100, y tiene 70 litros : 1000 km
 		double autonomia = coche.getCapacidad() / coche.getGastoCombustible() * 100;
 		return autonomia;
+	}
+
+	public static double calculoVelocidad(Coche coche, int segundos) {
+		// Fórmula velocidad: aceleracion por el tiempo, si pasa de velocidad maxima se limitará a esta misma
+		double velocidad = coche.getAceleracion() * segundos;
+		if (velocidad > coche.getVelocidadMaxima()) {
+			velocidad = coche.getVelocidadMaxima();
+		}
+		return velocidad;
 	}
 
 }
--- src/test/TestCoche.java
+++ src/test/TestCoche.java
@@ -9,8 +9,16 @@ class TestCoche {
 
 	@Test
 	void testCalculoAutonomia() {
-		Coche c1 = new Coche("Toyota","Azul",50,"Coupe", 10);
-		assertEquals(OperacionesCoche.calculoAutonomia(c1), 500);
+		Coche c1 = new Coche("Toyota","Azul",50,"Coupe", 10, 20, 120);
+		assertEquals(500,OperacionesCoche.calculoAutonomia(c1));
+	}
+	
+	@Test
+	void testCalculoVelocidad() {
+		Coche c1 = new Coche("Toyota","Azul",50,"Coupe", 10, 15, 120);
+		assertEquals(120, OperacionesCoche.calculoVelocidad(c1, 50));
+		assertEquals(30, OperacionesCoche.calculoVelocidad(c1, 2));
+		assertEquals(60, OperacionesCoche.calculoVelocidad(c1, 4));
 	}
 
 }
